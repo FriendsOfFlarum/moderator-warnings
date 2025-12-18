f059f48064f5cf42decc99ae18e232a0c47e2c36
IanM <ian@morland.me>
2025-12-18 10:39:53 +0000

    fix: formatter sometimes not available

--- migrations/2020_05_31_000000_format_note_content_for_renderer.php
+++ migrations/2020_05_31_000000_format_note_content_for_renderer.php
@@ -10,17 +10,31 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use Flarum\Formatter\Formatter;
 use FoF\ModeratorWarnings\Model\Warning;
+use Illuminate\Container\Container;
 use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $formatter = Warning::getFormatter();
+        $container = Container::getInstance();
+
+        // Get the formatter from the container
+        if (!$container->bound(Formatter::class)) {
+            // If formatter is not available, skip migration
+            return;
+        }
+
+        $formatter = $container->make(Formatter::class);
 
         Warning::chunkById(1000, function ($warnings) use ($formatter) {
             foreach ($warnings as $warning) {
-                $warning->public_comment = $formatter->parse($warning->public_comment);
-                $warning->private_comment = $formatter->parse($warning->private_comment);
+                if ($warning->public_comment) {
+                    $warning->public_comment = $formatter->parse($warning->public_comment);
+                }
+                if ($warning->private_comment) {
+                    $warning->private_comment = $formatter->parse($warning->private_comment);
+                }
                 $warning->save();
             }
         });
